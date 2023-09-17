494ca78523ba7af67a9250cc4d428a91f2216506
Swapnil Jayant Kumar <skuma172@jaguarlandrover.com>
2023-09-17 20:17:01 +0530

    Update implementation of SVM
    The number of hyper-parameter variations are reduced for the bank dataset beacuse training takes a lot of time.

--- src/main_svm.py
+++ src/main_svm.py
@@ -18,3 +18,123 @@ if len(sys.argv) == 2:
         execute_rice = False
     else:
         print(f"Warning: '{sys.argv[1]}' dataset is not supported. Reverting to default behaviour.")
+
+if execute_rice:
+    # Rice Dataset
+    # ------------
+    # Boosting: kernel = "rbf"
+    print("\nDataset: Rice | Algorithm: SVM")
+    print("================================")
+    print("Hyperparameters")
+    print("---------------")
+    print("- kernel = 'rbf'")
+    print("Training")
+    print("--------")
+    train_metrics = Trainer()(
+        data_name="rice",
+        k=5,
+        train_fracs=np.linspace(0.1, 1, 30),
+        model_type="svm",
+        kernel="rbf",
+        probability=True,
+        random_state=1693854383
+        )
+    PlotTrainMetrices()(train_metrics=train_metrics)
+    print("Testing")
+    print("-------")
+    Tester()(data_name="rice", train_metrics=train_metrics)
+    # Boosting: kernel = "poly", degree = 1
+    print("\nDataset: Rice | Algorithm: SVM")
+    print("================================")
+    print("Hyperparameters")
+    print("---------------")
+    print("- kernel = 'poly'")
+    print("- degree = 1")
+    print("Training")
+    print("--------")
+    train_metrics = Trainer()(
+        data_name="rice",
+        k=5,
+        train_fracs=np.linspace(0.1, 1, 30),
+        model_type="svm",
+        kernel="poly",
+        degree=1,
+        probability=True,
+        random_state=1693854383
+        )
+    PlotTrainMetrices()(train_metrics=train_metrics)
+    print("Testing")
+    print("-------")
+    Tester()(data_name="rice", train_metrics=train_metrics)
+    # Boosting: kernel = "poly", degree = 3
+    print("\nDataset: Rice | Algorithm: SVM")
+    print("================================")
+    print("Hyperparameters")
+    print("---------------")
+    print("- kernel = 'poly'")
+    print("- degree = 3")
+    print("Training")
+    print("--------")
+    train_metrics = Trainer()(
+        data_name="rice",
+        k=5,
+        train_fracs=np.linspace(0.1, 1, 30),
+        model_type="svm",
+        kernel="poly",
+        degree=3,
+        probability=True,
+        random_state=1693854383
+        )
+    PlotTrainMetrices()(train_metrics=train_metrics)
+    print("Testing")
+    print("-------")
+    Tester()(data_name="rice", train_metrics=train_metrics)
+
+
+if execute_bank:
+    # Bank Dataset
+    # ------------
+    # Boosting: kernel = "rbf"
+    print("\nDataset: Bank | Algorithm: SVM")
+    print("================================")
+    print("Hyperparameters")
+    print("---------------")
+    print("- kernel = 'rbf'")
+    print("Training")
+    print("--------")
+    train_metrics = Trainer()(
+        data_name="bank",
+        k=5,
+        train_fracs=np.linspace(0.1, 1, 10),
+        model_type="svm",
+        kernel="rbf",
+        probability=True,
+        random_state=1693854383
+        )
+    PlotTrainMetrices()(train_metrics=train_metrics)
+    print("Testing")
+    print("-------")
+    Tester()(data_name="bank", train_metrics=train_metrics)
+    # Boosting: kernel = "poly", degree = 1
+    print("\nDataset: Bank | Algorithm: SVM")
+    print("================================")
+    print("Hyperparameters")
+    print("---------------")
+    print("- kernel = 'poly'")
+    print("- degree = 1")
+    print("Training")
+    print("--------")
+    train_metrics = Trainer()(
+        data_name="bank",
+        k=5,
+        train_fracs=np.linspace(0.1, 1, 10),
+        model_type="svm",
+        kernel="poly",
+        degree = 1,
+        probability=True,
+        random_state=1693854383
+        )
+    PlotTrainMetrices()(train_metrics=train_metrics)
+    print("Testing")
+    print("-------")
+    Tester()(data_name="bank", train_metrics=train_metrics)
