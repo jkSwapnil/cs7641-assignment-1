3a322961b942bc6945f505b6138002f155845e3e
Swapnil Jayant Kumar <skuma172@jaguarlandrover.com>
2023-09-17 20:18:58 +0530

    Update decision tree classifier
    Update the hyper-parameter for training for rice dataset

--- src/main_decision_tree.py
+++ src/main_decision_tree.py
@@ -66,12 +66,12 @@ if execute_rice:
     print("Testing")
     print("-------")
     Tester()(data_name="rice", train_metrics=train_metrics)
-    # Decision tree classifier: min_samples_leaf = 1, ccp_alpha = 0.0005
+    # Decision tree classifier: min_samples_leaf = 20, ccp_alpha = 0.01
     print("\nDataset: Rice | Algorithm: Decision Tree")
     print("========================================")
     print("Hyperparameters")
     print("---------------")
-    print("- min_samples_leaf = 1")
+    print("- min_samples_leaf = 20")
     print("- ccp_alpha = 0.01")
     print("Training")
     print("--------")
@@ -80,30 +80,8 @@ if execute_rice:
         k=5,
         train_fracs=np.linspace(0.1, 1, 30),
         model_type="decision_tree",
-        min_samples_leaf=1,
-        ccp_alpha=0.0005,
-        random_state=1693854383,
-        )
-    PlotTrainMetrices()(train_metrics=train_metrics)
-    print("Testing")
-    print("-------")
-    Tester()(data_name="rice", train_metrics=train_metrics)
-    # Decision tree classifier: min_samples_leaf = 10, ccp_alpha = 0.002
-    print("\nDataset: Rice | Algorithm: Decision Tree")
-    print("========================================")
-    print("Hyperparameters")
-    print("---------------")
-    print("- min_samples_leaf = 10")
-    print("- ccp_alpha = 0.002")
-    print("Training")
-    print("--------")
-    train_metrics = Trainer()(
-        data_name="rice",
-        k=5,
-        train_fracs=np.linspace(0.1, 1, 30),
-        model_type="decision_tree",
-        min_samples_leaf=10,
-        ccp_alpha=0.002,
+        min_samples_leaf=20,
+        ccp_alpha=0.01,
         random_state=1693854383,
         )
     PlotTrainMetrices()(train_metrics=train_metrics)
@@ -111,6 +89,7 @@ if execute_rice:
     print("-------")
     Tester()(data_name="rice", train_metrics=train_metrics)
     print()
+
 
 if execute_bank:
     # Bank Dataset
